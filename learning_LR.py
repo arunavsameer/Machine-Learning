dd758c8834145b1704b07fa9b13a7fb41648a0c6
Arunav Sameer <97653048+arunavsameer@users.noreply.github.com>
2024-03-11 00:41:29 +0530

    Update learning_LR.py
    improved derivative functions and added the cost function realization curve

--- learning_LR.py
+++ learning_LR.py
@@ -1,3 +1,8 @@
+import matplotlib.pyplot as plt 
+
+plot_x = []
+plot_y = []
+
 data_x = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 data_y = [3, 5, 7, 9, 11, 13, 15, 17, 19]
 
@@ -13,22 +18,38 @@ def j_wb(w, b):
     return mean_sq_err
 
 def derv_w_j_wb(w, b):
-    return (j_wb(w + h, b) - j_wb(w, b))/ h
+    delta_sum = 0;
+    for i in range(data_count):
+        delta_sum += ((((w * data_x[i]) + b) - data_y[i]) * data_x[i])
+    return (delta_sum / data_count)
 
 def derv_b_j_wb(w, b):
-    return (j_wb(w, b + h) - j_wb(w, b)) / h
+    delta_sum = 0;
+    for i in range(data_count):
+        delta_sum += (((w * data_x[i]) + b) - data_y[i])
+    return (delta_sum / data_count)
 
 def minimise_j_wb():
     w = 0
     b = 0
     err_w = 1
     err_b = 1
+    count = 0
     while(abs(err_w) > 0.00000000001 and abs(err_b) > 0.00000000001):
         err_w = alpha * derv_w_j_wb(w, b)
         err_b = alpha * derv_b_j_wb(w, b)
         w = w - err_w
         b = b - err_b
         #print(f"{err_w} {err_b}")
+        count+=1
+        if count%2000 == 0:
+            plot_x.append(count)
+            plot_y.append(j_wb(w, b))
     print(f"w: {w}\nb: {b}")
+    plt.plot(plot_x, plot_y)
+    plt.title("cost vs iteration")
+    plt.ylabel("cost(j_wb)")
+    plt.xlabel("iteration")
+    plt.show()
 
 minimise_j_wb()
