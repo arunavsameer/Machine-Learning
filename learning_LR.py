6532bb8473e6bad9c10bbabe0a8f5ba45f12d06d
Arunav Sameer <97653048+arunavsameer@users.noreply.github.com>
2024-03-10 13:35:29 +0530

    Create learning_LR.py

---
+++ learning_LR.py
@@ -0,0 +1,34 @@
+data_x = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+data_y = [3, 5, 7, 9, 11, 13, 15, 17, 19]
+
+data_count = len(data_x)
+alpha = 0.0001
+h = 0.0000001
+
+def j_wb(w, b):
+    sq_err = 0
+    for i in range(data_count):
+        sq_err += ((w * data_x[i]) + b - data_y[i]) * ((w * data_x[i]) + b - data_y[i])
+    mean_sq_err = sq_err / (2 * data_count)
+    return mean_sq_err
+
+def derv_w_j_wb(w, b):
+    return (j_wb(w + h, b) - j_wb(w, b))/ h
+
+def derv_b_j_wb(w, b):
+    return (j_wb(w, b + h) - j_wb(w, b)) / h
+
+def minimise_j_wb():
+    w = 0
+    b = 0
+    err_w = 1
+    err_b = 1
+    while(abs(err_w) > 0.00000000001 and abs(err_b) > 0.00000000001):
+        err_w = alpha * derv_w_j_wb(w, b)
+        err_b = alpha * derv_b_j_wb(w, b)
+        w = w - err_w
+        b = b - err_b
+        #print(f"{err_w} {err_b}")
+    print(f"w: {w}\nb: {b}")
+
+minimise_j_wb()
