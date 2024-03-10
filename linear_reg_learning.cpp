bce0b7dce755ce16c23f2e5ee8ef65eaab0fc7e9
Arunav Sameer <97653048+arunavsameer@users.noreply.github.com>
2024-03-10 13:34:23 +0530

    Update linear_reg_learning.cpp
    eliminated tmp_w and tmp_b (no use) and made the parameters more accurate

--- linear_reg_learning.cpp
+++ linear_reg_learning.cpp
@@ -1,49 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
-//try same in python also
-#define data_count 5
-#define alpha 0.01 //learning rate
-#define h 0.001 //dw and db
-#define ok_err 0.001 //passable error
 
-float w = 0;
-float b = 0;
+#define data_count 9
+#define alpha 0.0001 //learning rate
+#define h 0.0000001 //dw and db
+//#define ok_err 0.001 //passable error
 
-float data_x[data_count] = {1, 2, 3, 4, 7};
-float data_y[data_count] = {3, 5, 7, 9, 11};
+double w = 0;
+double b = 0;
 
-float j_wb(float w, float b){
-    float sq_err = 0;
+double data_x[data_count] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+double data_y[data_count] = {3, 5, 7, 9, 11, 13, 15, 17, 19};
+
+double j_wb(double w, double b){
+    double sq_err = 0;
     for(int i = 0; i < data_count; i++){
         sq_err += ((w * data_x[i]) + b - data_y[i]) * ((w * data_x[i]) + b - data_y[i]);
     }
-    float mean_sq_err = sq_err / (2 * data_count);
+    double mean_sq_err = sq_err / (2 * data_count);
     return mean_sq_err;
 }
 
-float derv_w_j_wb(float w, float b){
-    float partial_derv = (j_wb(w + h, b) - j_wb(w, b)) / h;
+double derv_w_j_wb(double w, double b){
+    double partial_derv = (j_wb(w + h, b) - j_wb(w, b)) / h;
     return partial_derv;
 }
 
-float derv_b_j_wb(float w, float b){
-    float partial_derv = (j_wb(w , b + h) - j_wb(w, b)) / h;
+double derv_b_j_wb(double w, double b){
+    double partial_derv = (j_wb(w , b + h) - j_wb(w, b)) / h;
     return partial_derv;
 }
 
 void minimize_j_wb(){
-    float tmp_w;
-    float tmp_b;
-    float err_w, err_b;
+    double tmp_w;
+    double tmp_b;
+    double err_w, err_b;
     do{
         err_w = ((alpha) * (derv_w_j_wb(w, b)));
         err_b = ((alpha) * (derv_b_j_wb(w, b)));
-        float tmp_w = w - err_w;
-        float tmp_b = b - err_b;
-        w = tmp_w;
-        b = tmp_b;
+        w = w - err_w;
+        b = b - err_b;
         //cout << err_w <<" " <<err_b <<endl;
-    }while(abs(err_w) > 0.0000001 && abs(err_b) > 0.0000001);
+    }while(abs(err_w) > 0.00000000001 && abs(err_b) > 0.00000000001);
 }
 
 int main(){
